89b314ff7e14ecc3c7eb70ff428629c2cf66e9a1
Angga Lanuma <lanuma.angga@gmail.com>
2022-09-13 23:49:16 +0700

    change base from hyperlocal
    Signed-off-by: Angga Lanuma <lanuma.angga@gmail.com>

---
+++ config/domain.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    [
+        'domains'=> ['demo.hyperlocal.kl-youniverse.com', 'demo.local.newshub.id', 'cnergy-fe.test'],
+        'token'=> 'c36df1565fc2898abba68f209bf4820f',
+        'namespace'=> 'DefaultSite'
+    ]
+];
