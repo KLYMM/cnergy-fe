5900cbc30def52dda413afa0eff751fd8d9be293
Edwin Immanuel Damanik <edwindamanik5@gmail.com>
2022-09-14 19:37:16 +0700

    fix: show trending tag

--- resources/views/ui/components/trending-tag.blade.php
+++ resources/views/ui/components/trending-tag.blade.php
@@ -1,10 +1,13 @@
-<div class="trending-tag-container">
-  <h4>trending tag #</h4>
-  <ul>
-    <li><a href="#">penangkapan patrialis akbar</a></li>
-    <li><a href="#">grasi antasari azhar</a></li>
-    <li><a href="#">plikada dki jakarta 2017</a></li>
-    <li><a href="#">sidang habieb rizieq</a></li>
-    <li><a href="#">sidang ahok</a></li>
-  </ul>
-</div>
+@if ($tag=collect(Data::trendingTag())->slice(0, 5)??null)
+  @if (count($tag)>0)
+    <div class="trending-tag-container">
+      <h4>trending tag #</h4>
+      <ul>
+        @foreach ($tag as $r)
+          <li><a href="{{Src::detailTag($r)}}">{{$r['title']??null}}</a></li>
+        @endforeach
+      </ul>
+    </div>
+  @endif
+@endif
+
