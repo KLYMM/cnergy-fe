33b41786835640755e9fc81a9e6b2553c85ef6b6
fadhilahumairah <fadhilahumairah24@gmail.com>
2022-09-16 13:03:45 +0700

    feat: add link to every slider card

--- resources/views/ui/components/slider-news.blade.php
+++ resources/views/ui/components/slider-news.blade.php
@@ -2,46 +2,46 @@
   <button class="pre-btn"><img src="{{ URL::asset('assets/icons/prev.svg') }}" alt=""></button>
   <button class="nxt-btn"><img src="{{ URL::asset('assets/icons/next.svg') }}" alt=""></button>
   <div class="slider-container">
-      <div class="slider-card">
+      <a href="#" class="slider-card">
           <div class="slider-image">
               <img src="{{ URL::asset('assets/images/example.webp') }}" class="slider-thumb" alt="">
           </div>
           <div class="slider-info">
               <span class="slider-title">asd</span>
           </div>
-      </div>
-      <div class="slider-card">
+      </a>
+      <a href="#" class="slider-card">
           <div class="slider-image">
               <img src="{{ URL::asset('assets/images/example.webp') }}" class="slider-thumb" alt="">
           </div>
           <div class="slider-info">
               <h2 class="slider-title">Ibu Kota Baru, Polri Rapatkan Satuan Kerja yang Bakal Pindah</h2>
           </div>
-      </div>
-      <div class="slider-card">
+      </a>
+      <a href="#" class="slider-card">
           <div class="slider-image">
               <img src="{{ URL::asset('assets/images/example.webp') }}" class="slider-thumb" alt="">
           </div>
           <div class="slider-info">
               <h2 class="slider-title">Ibu Kota Baru, Polri Rapatkan Satuan Kerja yang Bakal Pindah </h2>
           </div>
-      </div>
-      <div class="slider-card">
+      </a>
+      <a href="#" class="slider-card">
           <div class="slider-image">
               <img src="{{ URL::asset('assets/images/example.webp') }}" class="slider-thumb" alt="">
           </div>
           <div class="slider-info">
               <h2 class="slider-title">Ibu Kota Baru, Polri Rapatkan Satuan Kerja yang Bakal Pindah </h2>
           </div>
-      </div>
-      <div class="slider-card">
+      </a>
+      <a href="#" class="slider-card">
           <div class="slider-image">
               <img src="{{ URL::asset('assets/images/example.webp') }}" class="slider-thumb" alt="">
           </div>
           <div class="slider-info">
               <h2 class="slider-title">Ibu Kota Baru, Polri Rapatkan Satuan Kerja yang Bakal Pindah </h2>
           </div>
-      </div>
+      </a>
   </div>
 </section>
 
