5fae7d6c029863707a7a248c80b8caf23a544ce8
fadhilahumairah <fadhilahumairah24@gmail.com>
2022-09-16 12:46:50 +0700

    fix: slider css

--- resources/views/ui/components/slider-news.blade.php
+++ resources/views/ui/components/slider-news.blade.php
@@ -1,64 +1,47 @@
-<div class="owl-carousel owl-theme">
-  <div class="sliderContent">
-    <img src="{{ URL::asset('assets/images/example.webp') }}" alt="example" width="100%" height="115px" />
-    <div class="time">32 menit yang lalu</div>
-    <div class="desc">
-      Sri Mulyani Hapus Denda 200 Persen, Setoran Pajak Bakal
-      Naik?
-    </div>
+<section class="custom-slider"> 
+  <button class="pre-btn"><img src="{{ URL::asset('assets/icons/prev.svg') }}" alt=""></button>
+  <button class="nxt-btn"><img src="{{ URL::asset('assets/icons/next.svg') }}" alt=""></button>
+  <div class="slider-container">
+      <div class="slider-card">
+          <div class="slider-image">
+              <img src="{{ URL::asset('assets/images/example.webp') }}" class="slider-thumb" alt="">
+          </div>
+          <div class="slider-info">
+              <span class="slider-title">asd</span>
+          </div>
+      </div>
+      <div class="slider-card">
+          <div class="slider-image">
+              <img src="{{ URL::asset('assets/images/example.webp') }}" class="slider-thumb" alt="">
+          </div>
+          <div class="slider-info">
+              <h2 class="slider-title">Ibu Kota Baru, Polri Rapatkan Satuan Kerja yang Bakal Pindah</h2>
+          </div>
+      </div>
+      <div class="slider-card">
+          <div class="slider-image">
+              <img src="{{ URL::asset('assets/images/example.webp') }}" class="slider-thumb" alt="">
+          </div>
+          <div class="slider-info">
+              <h2 class="slider-title">Ibu Kota Baru, Polri Rapatkan Satuan Kerja yang Bakal Pindah </h2>
+          </div>
+      </div>
+      <div class="slider-card">
+          <div class="slider-image">
+              <img src="{{ URL::asset('assets/images/example.webp') }}" class="slider-thumb" alt="">
+          </div>
+          <div class="slider-info">
+              <h2 class="slider-title">Ibu Kota Baru, Polri Rapatkan Satuan Kerja yang Bakal Pindah </h2>
+          </div>
+      </div>
+      <div class="slider-card">
+          <div class="slider-image">
+              <img src="{{ URL::asset('assets/images/example.webp') }}" class="slider-thumb" alt="">
+          </div>
+          <div class="slider-info">
+              <h2 class="slider-title">Ibu Kota Baru, Polri Rapatkan Satuan Kerja yang Bakal Pindah </h2>
+          </div>
+      </div>
   </div>
+</section>
 
-  <div class="sliderContent">
-    <img src="{{ URL::asset('assets/images/example.webp') }}" alt="example" width="100%" height="115px" />
-    <div class="time">32 menit yang lalu</div>
-    <div class="desc">
-      Sri Mulyani Hapus Denda 200 Persen, Setoran Pajak Bakal
-      Naik?
-    </div>
-  </div>
-
-  <div class="sliderContent">
-    <img src="{{ URL::asset('assets/images/example.webp') }}" alt="example" width="100%" height="115px" />
-    <div class="time">32 menit yang lalu</div>
-    <div class="desc">
-      Sri Mulyani Hapus Denda 200 Persen, Setoran Pajak Bakal
-      Naik?
-    </div>
-  </div>
-
-  <div class="sliderContent">
-    <img src="{{ URL::asset('assets/images/example.webp') }}" alt="example" width="100%" height="115px" />
-    <div class="time">32 menit yang lalu</div>
-    <div class="desc">
-      Sri Mulyani Hapus Denda 200 Persen, Setoran Pajak Bakal
-      Naik?
-    </div>
-  </div>
-
-  <div class="sliderContent">
-    <img src="{{ URL::asset('assets/images/example.webp') }}" alt="example" width="100%" height="115px" />
-    <div class="time">32 menit yang lalu</div>
-    <div class="desc">
-      Sri Mulyani Hapus Denda 200 Persen, Setoran Pajak Bakal
-      Naik?
-    </div>
-  </div>
-
-  <div class="sliderContent">
-    <img src="{{ URL::asset('assets/images/example.webp') }}" alt="example" width="100%" height="115px" />
-    <div class="time">32 menit yang lalu</div>
-    <div class="desc">
-      Sri Mulyani Hapus Denda 200 Persen, Setoran Pajak Bakal
-      Naik?
-    </div>
-  </div>
-
-  <div class="sliderContent">
-    <img src="{{ URL::asset('assets/images/example.webp') }}" alt="example" width="100%" height="115px" />
-    <div class="time">32 menit yang lalu</div>
-    <div class="desc">
-      Sri Mulyani Hapus Denda 200 Persen, Setoran Pajak Bakal
-      Naik?
-    </div>
-  </div>
-</div>
