68bf8e8e3f2f9a514a50834b695d3e06b3277394
megis <atanmegis.cn@gmail.com>
2022-11-02 15:42:59 +0700

    orderlist section

--- resources/views/defaultsite/mobile-v2/components/section-list-news.blade.php
+++ resources/views/defaultsite/mobile-v2/components/section-list-news.blade.php
@@ -43,45 +43,38 @@
         </label>
     </div>
     <div class="container-section">
-        <div class="item-section">
+        <div data-section="1" class="item-section">
             <article>
-                <h2 class="pt-2">#FerdySambo</h2>
-                <h1>Ibunda Brigadir J pada Ferdy Sambo: Hancur Hatiku Pak, Segeralah Sadar dan Bertobat</h1>
-                <span>5 menit yang lalu</span>
-                <figure>
-                    <img src="https://cdns.klimg.com/merdeka.com/i/w/news/2022/10/26/1485887/670x335/sekuriti-komplek-ferdy-sambo-ceritakan-detik-detik-dvr-cctv-diganti-polisi.jpg"
-                        alt="images" width="375" height="225">
-                </figure>
-                <p>Amsal Sampetondok, menghadiri sidang lanjutan keponakannya yakni Ferdy Sambo di Pengadilan Negeri
-                    Jakarta Selatan, Rabu (26/10). Sidang beragendakan putusan sela dibacakan majelis hakim atas eksepsi
-                    diajukan tim kuasa hukum Ferdy Sambo.</p>
+                <h2 class="pt-2 pb-0 my-1">#FerdySambo</h2>
+                <div class="content-title my-1">
+                    <h1>Ibunda Brigadir J pada Ferdy Sambo: Hancur Hatiku Pak, Segeralah Sadar dan Bertobat</h1>
+                    <span>5 menit yang lalu</span>
+                </div>
+                <div class="content-image my-1">
+                    <figure>
+                        <img src="https://cdns.klimg.com/merdeka.com/i/w/news/2022/10/26/1485887/670x335/sekuriti-komplek-ferdy-sambo-ceritakan-detik-detik-dvr-cctv-diganti-polisi.jpg"
+                            alt="images" width="375" height="225">
+                    </figure>
+                </div>
+                <div class="content-desc my-1">
+                    <p>Amsal Sampetondok, menghadiri sidang lanjutan keponakannya yakni Ferdy Sambo di Pengadilan Negeri
+                        Jakarta Selatan, Rabu (26/10). Sidang beragendakan putusan sela dibacakan majelis hakim atas
+                        eksepsi
+                        diajukan tim kuasa hukum Ferdy Sambo.</p>
+                </div>
             </article>
             <div class="button-section p-3">
                 <a class="read-more" href="#">Selengkapnya<i class="fa-solid fa-arrow-right ms-1"></i></a>
-                <a class="like-button" href="#"><i class="fa-regular fa-heart"></i></a>
-                <a class="share-button" href="#"><i class="fa-sharp fa-solid fa-share-nodes"></i></a>
+                <a class="like-button" href="#"><i class="fa-regular fa-heart fs-2"></i></a>
+                <a class="share-button" href="#"><i class="fa-sharp fa-solid fa-share-nodes fs-2"></i></a>
             </div>
         </div>
-        <div class="item-section">
-            <article>
-                <h2>#FerdySambo</h2>
-                <h1>Sekuriti Komplek Ferdy Sambo Ceritakan Detik-Detik DVR CCTV Diganti Polisi</h1>
-                <span>5 menit yang lalu</span>
-                <figure>
-                    <img src="https://cdns.klimg.com/merdeka.com/i/w/news/2022/10/26/1485887/670x335/sekuriti-komplek-ferdy-sambo-ceritakan-detik-detik-dvr-cctv-diganti-polisi.jpg"
-                        alt="images" width="375" height="225">
-                </figure>
-                <p>Amsal Sampetondok, menghadiri sidang lanjutan keponakannya yakni Ferdy Sambo di Pengadilan Negeri
-                    Jakarta Selatan, Rabu (26/10). Sidang beragendakan putusan sela dibacakan majelis hakim atas eksepsi
-                    diajukan tim kuasa hukum Ferdy Sambo.</p>
-            </article>
-            <div class="button-section p-3">
-                <a class="read-more" href="#">Selengkapnya<i class="fa-solid fa-arrow-right ms-1"></i></a>
-                <a class="like-button" href="#"><i class="fa-regular fa-heart"></i></a>
-                <a class="share-button" href="#"><i class="fa-sharp fa-solid fa-share-nodes"></i></a>
-            </div>
-        </div>
-        <div class="item-section">
+        @include('defaultsite.mobile-v2.components.section-list-news2')
+        @include('defaultsite.mobile-v2.components.section-list-news3')
+        @include('defaultsite.mobile-v2.components.section-list-news4')
+        @include('defaultsite.mobile-v2.components.section-list-news5')
+
+        {{-- <div data-section="tai" class="item-section">
             <article>
                 <h2>#FerdySambo</h2>
                 <h1>Sekuriti Komplek Ferdy Sambo Ceritakan Detik-Detik DVR CCTV Diganti Polisi</h1>
@@ -99,7 +92,26 @@
                 <a class="like-button" href="#"><i class="fa-regular fa-heart"></i></a>
                 <a class="share-button" href="#"><i class="fa-sharp fa-solid fa-share-nodes"></i></a>
             </div>
-        </div>
+        </div> --}}
+        {{-- <div class="item-section">
+            <article>
+                <h2>#FerdySambo</h2>
+                <h1>Sekuriti Komplek Ferdy Sambo Ceritakan Detik-Detik DVR CCTV Diganti Polisi</h1>
+                <span>5 menit yang lalu</span>
+                <figure>
+                    <img src="https://cdns.klimg.com/merdeka.com/i/w/news/2022/10/26/1485887/670x335/sekuriti-komplek-ferdy-sambo-ceritakan-detik-detik-dvr-cctv-diganti-polisi.jpg"
+                        alt="images" width="375" height="225">
+                </figure>
+                <p>Amsal Sampetondok, menghadiri sidang lanjutan keponakannya yakni Ferdy Sambo di Pengadilan Negeri
+                    Jakarta Selatan, Rabu (26/10). Sidang beragendakan putusan sela dibacakan majelis hakim atas eksepsi
+                    diajukan tim kuasa hukum Ferdy Sambo.</p>
+            </article>
+            <div class="button-section p-3">
+                <a class="read-more" href="#">Selengkapnya<i class="fa-solid fa-arrow-right ms-1"></i></a>
+                <a class="like-button" href="#"><i class="fa-regular fa-heart"></i></a>
+                <a class="share-button" href="#"><i class="fa-sharp fa-solid fa-share-nodes"></i></a>
+            </div>
+        </div> --}}
     </div>
 
 </section>
---
+++ resources/views/defaultsite/mobile-v2/components/section-list-news5.blade.php
@@ -0,0 +1,26 @@
+<div data-section="5" class="item-section">
+    <article>
+        <h2 class="py-1 my-1">#FerdySambo</h2>
+        <div class="content-title my-1">
+            <h1>Ibunda Brigadir J pada Ferdy Sambo: Hancur Hatiku Pak, Segeralah Sadar dan Bertobat</h1>
+            <span>5 menit yang lalu</span>
+        </div>
+        <div class="content-image my-1">
+            <figure>
+                <img src="https://cdns.klimg.com/merdeka.com/i/w/news/2022/10/26/1485887/670x335/sekuriti-komplek-ferdy-sambo-ceritakan-detik-detik-dvr-cctv-diganti-polisi.jpg"
+                    alt="images" width="375" height="225">
+            </figure>
+        </div>
+        <div class="content-desc my-1">
+            <p>Amsal Sampetondok, menghadiri sidang lanjutan keponakannya yakni Ferdy Sambo di Pengadilan Negeri
+                Jakarta Selatan, Rabu (26/10). Sidang beragendakan putusan sela dibacakan majelis hakim atas
+                eksepsi
+                diajukan tim kuasa hukum Ferdy Sambo.</p>
+        </div>
+    </article>
+    <div class="button-section p-3">
+        <a class="read-more" href="#">Selengkapnya<i class="fa-solid fa-arrow-right ms-1"></i></a>
+        <a class="like-button" href="#"><i class="fa-regular fa-heart fs-2"></i></a>
+        <a class="share-button" href="#"><i class="fa-sharp fa-solid fa-share-nodes fs-2"></i></a>
+    </div>
+</div>
