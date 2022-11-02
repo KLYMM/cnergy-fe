7b7f5f9d5ce2ade830384af114358a2e51e44a04
HafizhHasanAR <hafizhhafizh9@gmail.com>
2022-11-02 13:52:37 +0700

    theme-3

--- resources/views/defaultsite/mobile-v2/components/section-list-news.blade.php
+++ resources/views/defaultsite/mobile-v2/components/section-list-news.blade.php
@@ -1,105 +1,77 @@
-<section>
-    <div class="section-tag">
-        <a href="#">
-            <img src="{{ URL::asset('assets/images/new-logo-mobile.webp') }}" width="132" height="41" />
-        </a>
-        <input class="switchTheme-control" type="checkbox" name="checkbox" id="switchTheme" autocomplete="off">
-        <label class="switchTheme-icon" for="switchTheme">
-            <svg class="dark:svg-both-white switchTheme-icon-dark" width="24" height="24" viewBox="0 0 24 24"
-                fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                    d="M21 12.79C20.8427 14.4922 20.2039 16.1144 19.1582 17.4668C18.1126 18.8192 16.7035 19.8458 15.0957 20.4265C13.4879 21.0073 11.748 21.1181 10.0795 20.7461C8.41102 20.3741 6.88299 19.5345 5.67423 18.3258C4.46546 17.117 3.62594 15.589 3.25391 13.9205C2.88188 12.252 2.99272 10.5121 3.57346 8.9043C4.1542 7.29651 5.18083 5.88737 6.53321 4.84175C7.88559 3.79614 9.5078 3.15731 11.21 3C10.2134 4.34827 9.73385 6.00945 9.85853 7.68141C9.98321 9.35338 10.7039 10.9251 11.8894 12.1106C13.0749 13.2961 14.6466 14.0168 16.3186 14.1415C17.9906 14.2662 19.6517 13.7866 21 12.79V12.79Z"
-                    fill="black" />
-            </svg>
-            <svg class="dark:svg-stroke-white switchTheme-icon-light" width="24" height="24" viewBox="0 0 24 24"
-                fill="none" xmlns="http://www.w3.org/2000/svg">
-                <g clip-path="url(#clip0_1_6)">
-                    <path
-                        d="M12 17C14.7614 17 17 14.7614 17 12C17 9.23858 14.7614 7 12 7C9.23858 7 7 9.23858 7 12C7 14.7614 9.23858 17 12 17Z"
-                        fill="none" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                    <path d="M12 1V3" stroke="black" stroke-width="2" stroke-linecap="round"
-                        stroke-linejoin="round" />
-                    <path d="M12 21V23" stroke="black" stroke-width="2" stroke-linecap="round"
-                        stroke-linejoin="round" />
-                    <path d="M4.22 4.22L5.64 5.64" stroke="black" stroke-width="2" stroke-linecap="round"
-                        stroke-linejoin="round" />
-                    <path d="M18.36 18.36L19.78 19.78" stroke="black" stroke-width="2" stroke-linecap="round"
-                        stroke-linejoin="round" />
-                    <path d="M1 12H3" stroke="black" stroke-width="2" stroke-linecap="round"
-                        stroke-linejoin="round" />
-                    <path d="M21 12H23" stroke="black" stroke-width="2" stroke-linecap="round"
-                        stroke-linejoin="round" />
-                    <path d="M4.22 19.78L5.64 18.36" stroke="black" stroke-width="2" stroke-linecap="round"
-                        stroke-linejoin="round" />
-                    <path d="M18.36 5.64L19.78 4.22" stroke="black" stroke-width="2" stroke-linecap="round"
-                        stroke-linejoin="round" />
-                </g>
-                <defs>
-                    <clipPath id="clip0_1_6">
-                        <rect width="24" height="24" fill="white" />
-                    </clipPath>
-                </defs>
-            </svg>
-        </label>
-    </div>
+
     <div class="container-section">
-        <div class="item-section">
-            <article>
-                <h2 class="pt-2">#FerdySambo</h2>
-                <h1>Ibunda Brigadir J pada Ferdy Sambo: Hancur Hatiku Pak, Segeralah Sadar dan Bertobat</h1>
-                <span>5 menit yang lalu</span>
-                <figure>
-                    <img src="https://cdns.klimg.com/merdeka.com/i/w/news/2022/10/26/1485887/670x335/sekuriti-komplek-ferdy-sambo-ceritakan-detik-detik-dvr-cctv-diganti-polisi.jpg"
-                        alt="images" width="375" height="225">
-                </figure>
-                <p>Amsal Sampetondok, menghadiri sidang lanjutan keponakannya yakni Ferdy Sambo di Pengadilan Negeri
-                    Jakarta Selatan, Rabu (26/10). Sidang beragendakan putusan sela dibacakan majelis hakim atas eksepsi
-                    diajukan tim kuasa hukum Ferdy Sambo.</p>
-            </article>
-            <div class="button-section p-3">
-                <a class="read-more" href="#">Selengkapnya<i class="fa-solid fa-arrow-right ms-1"></i></a>
-                <a class="like-button" href="#"><i class="fa-regular fa-heart"></i></a>
-                <a class="share-button" href="#"><i class="fa-sharp fa-solid fa-share-nodes"></i></a>
+        <div class="item-section-1">
+            <div class="navbar-item d-flex"  >
+                <a href="#" class="header-right" style="margin: 20px;">
+                    <img src="{{ URL::asset('assets/images/logo-new.png') }}" width="132" height="41" /> 
+                </a>
+                <div class="header-left" style="margin: 15px 0px 0px 163px" >
+                    <svg class="dark:svg-stroke-white switchTheme-icon-light" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g clip-path="url(#clip0_1_6)">
+                            <path d="M12 17C14.7614 17 17 14.7614 17 12C17 9.23858 14.7614 7 12 7C9.23858 7 7 9.23858 7 12C7 14.7614 9.23858 17 12 17Z" fill="none" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M12 1V3" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M12 21V23" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M4.22 4.22L5.64 5.64" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M18.36 18.36L19.78 19.78" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M1 12H3" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M21 12H23" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M4.22 19.78L5.64 18.36" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M18.36 5.64L19.78 4.22" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </g>
+                        <defs>
+                            <clipPath id="clip0_1_6">
+                                <rect width="24" height="24" fill="white"/>
+                            </clipPath>
+                        </defs>
+                    </svg>
+                </div>
+            </div>
+            <h1 class="item_img" ><span class="item_p" >#Ferdy Sambo</span></h1>    
+            <figure class="item-image" >
+                <img  src="https://cdns.klimg.com/merdeka.com/i/w/news/2022/10/26/1485927/670x335/tim-siber-polri-dvr-cctv-di-kompleks-duren-tiga-kosong-saat-diperiksa-puslabfor.jpg" alt="images" width="100%" height="100%" style="object-fit: cover">
+            </figure>
+            <div class="article_main">
+                <h1 class="article-title" >Tim Siber Polri: DVR CCTV di Kompleks Duren Tiga Kosong saat Diperiksa Puslabfor</h1>
+                <span class="article-date ms-2 " style="font-size: 11px" >5 menit yang lalu</span>
+             <div class="article-paragraph mt-4" style="font-size: 15px">
+                <p>Saat itu, Rabu 13 Juli 2022 atau lima hari usai penembakan Brigadir J di rumah dinas Ferdy Sambo, Kompol Baiquni, Kompol Chuck Putranto, AKBP Arif Rahman dan AKBP Ridwan Soplanit.</p>
             </div>
         </div>
-        <div class="item-section">
-            <article>
-                <h2>#FerdySambo</h2>
-                <h1>Sekuriti Komplek Ferdy Sambo Ceritakan Detik-Detik DVR CCTV Diganti Polisi</h1>
-                <span>5 menit yang lalu</span>
-                <figure>
-                    <img src="https://cdns.klimg.com/merdeka.com/i/w/news/2022/10/26/1485887/670x335/sekuriti-komplek-ferdy-sambo-ceritakan-detik-detik-dvr-cctv-diganti-polisi.jpg"
-                        alt="images" width="375" height="225">
-                </figure>
-                <p>Amsal Sampetondok, menghadiri sidang lanjutan keponakannya yakni Ferdy Sambo di Pengadilan Negeri
-                    Jakarta Selatan, Rabu (26/10). Sidang beragendakan putusan sela dibacakan majelis hakim atas eksepsi
-                    diajukan tim kuasa hukum Ferdy Sambo.</p>
-            </article>
-            <div class="button-section p-3">
-                <a class="read-more" href="#">Selengkapnya<i class="fa-solid fa-arrow-right ms-1"></i></a>
-                <a class="like-button" href="#"><i class="fa-regular fa-heart"></i></a>
-                <a class="share-button" href="#"><i class="fa-sharp fa-solid fa-share-nodes"></i></a>
+        <div class="article-footer d-flex text-center " style="margin:235px 20px;">
+            <div class="article-footer-left ">
+                <a class="btn btn-outline-purpule" href="/">
+                    <span>Selengkapnya</span>
+                    <svg class="" width="15" height="15" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M2 12H22" stroke="#551575" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M15 5L22 12L15 19" stroke="#551575" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                </a>
             </div>
-        </div>
-        <div class="item-section">
-            <article>
-                <h2>#FerdySambo</h2>
-                <h1>Sekuriti Komplek Ferdy Sambo Ceritakan Detik-Detik DVR CCTV Diganti Polisi</h1>
-                <span>5 menit yang lalu</span>
-                <figure>
-                    <img src="https://cdns.klimg.com/merdeka.com/i/w/news/2022/10/26/1485887/670x335/sekuriti-komplek-ferdy-sambo-ceritakan-detik-detik-dvr-cctv-diganti-polisi.jpg"
-                        alt="images" width="375" height="225">
-                </figure>
-                <p>Amsal Sampetondok, menghadiri sidang lanjutan keponakannya yakni Ferdy Sambo di Pengadilan Negeri
-                    Jakarta Selatan, Rabu (26/10). Sidang beragendakan putusan sela dibacakan majelis hakim atas eksepsi
-                    diajukan tim kuasa hukum Ferdy Sambo.</p>
-            </article>
-            <div class="button-section p-3">
-                <a class="read-more" href="#">Selengkapnya<i class="fa-solid fa-arrow-right ms-1"></i></a>
-                <a class="like-button" href="#"><i class="fa-regular fa-heart"></i></a>
-                <a class="share-button" href="#"><i class="fa-sharp fa-solid fa-share-nodes"></i></a>
+            <div class="article-footer-right ">
+                <div class="article-footer-group mx-4">
+                    <div class="article-footer-group-item ">
+                        <a class="article-footer-group-item-btn " href="">
+                            <svg class="" width="23" height="22" viewBox="0 0 23 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M20.34 3.60999C19.8292 3.099 19.2228 2.69364 18.5554 2.41708C17.8879 2.14052 17.1725 1.99817 16.45 1.99817C15.7275 1.99817 15.0121 2.14052 14.3446 2.41708C13.6772 2.69364 13.0708 3.099 12.56 3.60999L11.5 4.66999L10.44 3.60999C9.4083 2.5783 8.00903 1.9987 6.55 1.9987C5.09096 1.9987 3.69169 2.5783 2.66 3.60999C1.6283 4.64169 1.04871 6.04096 1.04871 7.49999C1.04871 8.95903 1.6283 10.3583 2.66 11.39L3.72 12.45L11.5 20.23L19.28 12.45L20.34 11.39C20.851 10.8792 21.2563 10.2728 21.5329 9.60535C21.8095 8.93789 21.9518 8.22248 21.9518 7.49999C21.9518 6.77751 21.8095 6.0621 21.5329 5.39464C21.2563 4.72718 20.851 4.12075 20.34 3.60999Z" stroke="#400060" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                            <span style="display:grid; font-size:11px;" >Suka</span>
+                        </a>
+                    </div>
+                </div>
             </div>
+          
+
+           
+           
+           
+            
         </div>
+        <div class="item-section">PAGE2</div>
+        {{-- <div class="item-section"> --}}
+            {{-- @include('defaultsite.mobile-v2.components.section-theme-3') --}}
+        {{-- </div> --}}
+        <div class="item-section">PAGE4</div>
+        <div class="item-section">PAGE5</div>
+        <div class="item-section">PAGE6</div>
     </div>
 
-</section>
