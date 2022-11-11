35bb3098aca0428db7d00ed56627d49a280abf37
Edwin Immanuel Damanik <edwindamanik5@gmail.com>
2022-11-11 14:20:59 +0700

    redesign readpage

--- resources/views/defaultsite/mobile-v2/layouts/main.blade.php
+++ resources/views/defaultsite/mobile-v2/layouts/main.blade.php
@@ -42,7 +42,7 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Nunito+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Red+Hat+Display:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
         rel="stylesheet" />
-    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400&display=swap" rel="stylesheet">
 
     <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" rel="stylesheet" />
 
