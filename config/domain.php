360f302558320ba89cb3b605a3de79427c250c62
Yuda Prawira <yuda.prawira@kly.id>
2022-11-22 07:52:29 +0000

    domain.php edited online with Bitbucket

--- config/domain.php
+++ config/domain.php
@@ -2,12 +2,12 @@
 
 return [
     [
-        'domains'=> ['demo.hyperlocal.kl-youniverse.com', 'demo.local.newshub.id', 'cnergy-fe.test', 'localhost', 'english-mm.kl-youniverse.com', 'www.staging.trstd.ly', 'staging.trstd.ly'],
+        'domains'=> ['demo.hyperlocal.kl-youniverse.com', 'demo.local.newshub.id', 'cnergy-fe.test', 'localhost', 'english-mm.kl-youniverse.com'],
         'token'=> env('NEWSHUB_TOKEN', ''),
         'namespace'=> 'DefaultSite'
     ],
     [
-        'domains' => ['www.trstd.ly', 'trstd.ly'],
+        'domains' => ['www.trstd.ly', 'trstd.ly', 'www.staging.trstd.ly', 'staging.trstd.ly'],
         'token' => env('NEWSHUB_TOKEN_TRSTDLY', ''),
         'namespace' => 'DefaultSite'
     ]
