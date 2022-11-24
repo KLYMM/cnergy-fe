25d349d949f6dbd116c56577b927d1b718af294a
Yuda Prawira <yuda.prawira@kly.id>
2022-11-24 06:41:36 +0000

    domain.php edited online with Bitbucket

--- config/domain.php
+++ config/domain.php
@@ -7,7 +7,7 @@ return [
         'namespace'=> 'DefaultSite'
     ],
     [
-        'domains' => ['www.trstd.ly', 'trstd.ly', 'www.staging.trstd.ly', 'staging.trstd.ly'],
+        'domains' => ['www.trstd.ly', 'trstd.ly', 'www.staging.trstd.ly', 'staging.trstd.ly', 'trstdly.com', 'www.trstdly.com', 'staging.trstdly.com', 'www.staging.trstdly.com'],
         'token' => env('NEWSHUB_TOKEN_TRSTDLY', ''),
         'namespace' => 'DefaultSite'
     ]
