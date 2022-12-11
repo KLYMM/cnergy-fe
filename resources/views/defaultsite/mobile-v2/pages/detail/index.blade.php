eb4574c4e2283a0380ab04f85211b4605f17d0b7
HafizhHasanAR <hafizhhafizh9@gmail.com>
2022-12-11 16:01:51 +0700

    case text img

--- resources/views/defaultsite/mobile-v2/pages/detail/index.blade.php
+++ resources/views/defaultsite/mobile-v2/pages/detail/index.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
     {{-- @dd($content[3]->ownerDocument->saveHtml($content[3]->firstChild)) --}}
-    @include('defaultsite.mobile-v2.pages.detail.components.imageCase.imageCase1', ['row' => $row])
+    {{-- @include('defaultsite.mobile-v2.pages.detail.components.imageCase.imageCase1', ['row' => $row])
     @foreach ($content->chunk(2) as $chunk)
         @if ($loop->odd)
             @include('defaultsite.mobile-v2.pages.detail.components.textCase.textCase1', [
@@ -12,9 +12,13 @@
         @else
             @include('defaultsite.mobile-v2.pages.detail.components.tiga', ['chunk' => $chunk])
         @endif
-    @endforeach
-    @include('defaultsite.mobile-v2.pages.detail.components.listCase.listCase1', ['row' => $row])
+    @endforeach --}}
+    {{-- list-case-sample --}}
+    {{-- @include('defaultsite.mobile-v2.pages.detail.components.listCase.listCase1', ['row' => $row])
     @include('defaultsite.mobile-v2.pages.detail.components.listCase.listCase2', ['row' => $row])
     @include('defaultsite.mobile-v2.pages.detail.components.listCase.listCase3', ['row' => $row])
-    @include('defaultsite.mobile-v2.pages.detail.components.listCase.listCase4', ['row' => $row])
+    @include('defaultsite.mobile-v2.pages.detail.components.listCase.listCase4', ['row' => $row]) --}}
+    {{-- text+photo sample --}}
+    @include('defaultsite.mobile-v2.pages.detail.components.textPhotocase.temp4', ['row' => $row])
+    @include('defaultsite.mobile-v2.pages.detail.components.textPhotocase.temp5', ['row' => $row])
 @endsection
