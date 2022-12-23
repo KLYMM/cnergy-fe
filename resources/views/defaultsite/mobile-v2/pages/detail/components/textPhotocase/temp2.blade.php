353dbabbd8c76064cbd9cc0f71ca3512bd855686
HafizhHasanAR <hafizhhafizh9@gmail.com>
2022-12-23 14:48:07 +0700

    fix detail content

--- resources/views/defaultsite/mobile-v2/pages/detail/components/textPhotocase/temp2.blade.php
+++ resources/views/defaultsite/mobile-v2/pages/detail/components/textPhotocase/temp2.blade.php
@@ -15,7 +15,7 @@
                 </div>
             </div>
             
-            <div class="dt-paras flex-1 mt-10 relative z-10 text-lg leading-relaxed font-karla  font-normal px-6 animate animate--fadeInUp" style="--delay: 0ms">
+            <div class="dt-para flex-1 mt-10 relative z-10 text-lg leading-relaxed font-karla  font-normal px-6 animate animate--fadeInUp" style="--delay: 0ms">
                 <p>{!! $chunk['attributes']['title'] !!} </p>
            </div>
          
@@ -23,6 +23,10 @@
             <div class="article-footer flex justify-center items-center py-6">
                 <span
                     class="article-swipeup font-normal vh-text-md animate-swipe animate-swipe-up text-base ">
+                    <svg class="transform rotate-180 inline-block -mt-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor">
+                        <path d="m12 15.586-4.293-4.293-1.414 1.414L12 18.414l5.707-5.707-1.414-1.414z"></path>
+                        <path d="m17.707 7.707-1.414-1.414L12 10.586 7.707 6.293 6.293 7.707 12 13.414z"></path>
+                    </svg>
                     SWIPE UP</span>
             </div>
         </article>
